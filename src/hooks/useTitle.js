e8aed08da98ce138566b7752fc4680bdba7c52a0
kbhargav03 <106007461+kbhargav03@users.noreply.github.com>
2023-08-13 12:53:04 +0530

    Add files via upload

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,1 +1,10 @@
+import { useEffect } from "react";
 
+export const useTitle = (title) => {
+
+    useEffect(() => {
+        document.title = `${title} - CodeBook`;
+    }, [title]);
+
+  return null;
+}
